529168e1f7910c959572ad400f8d63c2fe53b531
Duleeka <dulikamadushani@gmail.com>
2019-03-04 20:36:01 +0530

    your commit message e.g. First commit

--- src/main/java/com/gn/app/controller/HomeController.java
+++ src/main/java/com/gn/app/controller/HomeController.java
@@ -1,5 +1,8 @@
-package com.gn.app.controller;
+package com.gn.app.controller.dashboard;
 
+import com.gn.app.model.Asset;
+import com.gn.app.service.AssetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +12,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Created by hp on 1/6/2019.
  */
 @Controller
+@RequestMapping(HomeController.REQUEST_MAPPING_URL)
 public class HomeController {
+
+    public static final String REQUEST_MAPPING_URL = "/dashboard";
+
+    @Autowired
+    AssetService assetService;
+
+
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String homePage(Model model) {
-      //model.addAttribute("person", new Person());
+    public String homePage(Model model){
+        //model.addAttribute("person", new Person());
+        Asset asset=new Asset();
+        asset.setName("dulika");
+        asset.setAssetNo("no");
         return "dashboard/index";
     }
-
 
 }
